5ac195b9a3770d365774dab80bffec5026076260
taiga07 <taigatani@me.com>
2023-06-15 21:28:37 +0900

    Initial Commit

---
+++ I AM POOR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM POOR
+//
+//  Created by 谷太海 on 2023/06/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
